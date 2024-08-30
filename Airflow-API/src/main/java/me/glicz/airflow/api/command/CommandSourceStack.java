e3d2f28ca66cd086c5f16e1bfc493d396b1c05ff
GliczDev <67753196+GliczDev@users.noreply.github.com>
2024-08-30 21:54:07 +0200

    Add CommandSourceStack#getTarget

--- Airflow-API/src/main/java/me/glicz/airflow/api/command/CommandSourceStack.java
+++ Airflow-API/src/main/java/me/glicz/airflow/api/command/CommandSourceStack.java
@@ -3,8 +3,14 @@ package me.glicz.airflow.api.command;
 import me.glicz.airflow.api.command.sender.CommandSender;
 import me.glicz.airflow.api.entity.Entity;
 
+import java.util.Objects;
+
 public interface CommandSourceStack {
     CommandSender getSender();
 
     Entity getExecutor();
+
+    default CommandSender getTarget() {
+        return Objects.requireNonNullElse(getExecutor(), getSender());
+    }
 }

5e549faaa6648e7d3f63a6aaf4d52b4ea8cc9ad4
GliczDev <67753196+GliczDev@users.noreply.github.com>
2024-09-22 12:59:35 +0200

    Fix AdventureUtils.FLATTENER

--- Airflow-Server/src/main/java/me/glicz/airflow/util/AdventureUtils.java
+++ Airflow-Server/src/main/java/me/glicz/airflow/util/AdventureUtils.java
@@ -1,6 +1,7 @@
 package me.glicz.airflow.util;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.ComponentLike;
 import net.kyori.adventure.text.TranslatableComponent;
 import net.kyori.adventure.text.TranslationArgument;
 import net.kyori.adventure.text.flattener.ComponentFlattener;
@@ -11,19 +12,57 @@ import net.kyori.adventure.translation.Translator;
 import net.minecraft.locale.Language;
 
 import java.util.Locale;
-import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class AdventureUtils {
+    // Copied FORMAT_SPECIFIER and FORMAT_SPECIFIER_PATTERN from java.util.Formatter
+    // %[argument_index$][flags][width][.precision][t]conversion
+    static final String FORMAT_SPECIFIER
+            = "%(\\d+\\$)?([-#+ 0,(\\<]*)?(\\d+)?(\\.\\d+)?([tT])?([a-zA-Z%])";
+    static final Pattern FORMAT_SPECIFIER_PATTERN = Pattern.compile(FORMAT_SPECIFIER);
+
     public static final ComponentFlattener FLATTENER = ComponentFlattener.basic().toBuilder()
             .complexMapper(TranslatableComponent.class, (component, consumer) -> {
                 if (Language.getInstance().has(component.key())) {
-                    String translated = Language.getInstance()
-                            .getOrDefault(component.key(), Objects.requireNonNullElse(component.fallback(), component.key()))
-                            .formatted(component.arguments().stream()
-                                    .map(TranslationArgument::value)
-                                    .toArray()
-                            );
-                    consumer.accept(Component.text(translated, component.style()));
+                    //noinspection DataFlowIssue
+                    String translated = Language.getInstance().getOrDefault(component.key(), component.fallback());
+                    //noinspection ConstantValue
+                    if (translated == null) {
+                        consumer.accept(Component.text(component.key()));
+                        return;
+                    }
+
+                    Object[] plainArgs = component.arguments().stream()
+                            .map(TranslationArgument::value)
+                            .toArray();
+
+                    Matcher matcher = FORMAT_SPECIFIER_PATTERN.matcher(translated);
+                    int lastArg = 0, lastIndex = 0;
+
+                    while (matcher.find()) {
+                        if (matcher.start() > lastIndex) {
+                            consumer.accept(Component.text(translated.substring(lastIndex, matcher.start())));
+                        }
+
+                        String index = matcher.group(1);
+                        int arg = index != null
+                                ? Integer.parseInt(index.substring(0, index.length() - 1))
+                                : lastArg++;
+
+                        TranslationArgument argument = component.arguments().get(arg);
+                        if (argument.value() instanceof ComponentLike componentLike) {
+                            consumer.accept(componentLike.asComponent());
+                        } else {
+                            consumer.accept(Component.text(matcher.group().formatted(plainArgs)));
+                        }
+
+                        lastIndex = matcher.end();
+                    }
+
+                    if (lastIndex < translated.length()) {
+                        consumer.accept(Component.text(translated.substring(lastIndex)));
+                    }
                     return;
                 }
 
